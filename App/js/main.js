50d0e40a71293e2233e7b7d6e800b7e656964bad
Azril Ardian Ifansyah <irfan05ariell@gmail.com>
2020-12-08 00:14:59 +0800

    Update: change registration

--- App/js/main.js
+++ App/js/main.js
@@ -2,9 +2,13 @@ const main = () => {
 	// Servie Workers Registration
 	if ("serviceWorker" in navigator) {
 		navigator.serviceWorker
-			.register("/service-worker.js")
-			.then(() => console.log("Succes!"))
-			.catch((err) => console.log("Failed!", err));
+			.register("service-worker.js")
+			.then(function (registration) {
+				console.log("Registered:", registration);
+			})
+			.catch(function (error) {
+				console.log("Registration failed: ", error);
+			});
 	}
 };
 
