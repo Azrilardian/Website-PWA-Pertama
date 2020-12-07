ccb364801e499c63e37fb509b0002635cec77050
Azril Ardian Ifansyah <irfan05ariell@gmail.com>
2020-12-08 00:42:36 +0800

    Update : build

--- App/index.js
+++ App/index.js
@@ -1,16 +1,3 @@
 import "./css/style.css";
 import main from "./js/main";
-
-// Servie Workers Registration
-if ("serviceWorker" in navigator) {
-	navigator.serviceWorker
-		.register("service-workers.js")
-		.then(function (registration) {
-			console.log("Registered:", registration);
-		})
-		.catch(function (error) {
-			console.log("Registration failed: ", error);
-		});
-}
-
 document.addEventListener("DOMContentLoaded", main);
--- App/js/main.js
+++ App/js/main.js
@@ -1,3 +1,15 @@
-const main = () => {};
+const main = () => {
+	// Servie Workers Registration
+	if ("serviceWorker" in navigator) {
+		navigator.serviceWorker
+			.register("service-worker.js")
+			.then(function (registration) {
+				console.log("Registered:", registration);
+			})
+			.catch(function (error) {
+				console.log("Registration failed: ", error);
+			});
+	}
+};
 
 export default main;
