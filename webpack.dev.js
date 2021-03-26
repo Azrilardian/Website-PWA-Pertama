0256e71cd23c85dcd4fb2071c1993bfedfd1b7f5
Azril Ardian Ifansyah <irfan05ariell@gmail.com>
2021-03-27 01:06:12 +0800

    Update : menambah file-loader package

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,4 +3,18 @@ const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
 	mode: "development",
+	module: {
+		rules: [
+			{
+				test: /\.(png|svg|jpg|jpeg)$/,
+				use: {
+					loader: "file-loader",
+					options: {
+						name: "[name].[ext]",
+						outputPath: "img",
+					},
+				},
+			},
+		],
+	},
 });
